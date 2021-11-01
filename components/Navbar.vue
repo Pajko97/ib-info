4d317cb2e73038badfc4a80dd1ccadf83d981396
pavle <pavle@git.topshipment.com>
2021-11-01 15:05:03 +0100

    No flags

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -11,10 +11,7 @@
             <li class="pr-8 text-white font-normal text-2xl"><NuxtLink to="/about">About us</NuxtLink></li>
             <li class="pr-8 text-white text-2xl"><NuxtLink to="/contact">Contact</NuxtLink></li>
         </ul>
-        <div class="max-w-xs">
-            <nuxt-link :to="switchLocalePath('hr')"><img src="../static/images/flag_croatia.png"/></nuxt-link>
-      <nuxt-link :to="switchLocalePath('es')"><img src="../static/images/flag_spain.png"/></nuxt-link>
-        </div>
+       
     </div>
 </template>
 
