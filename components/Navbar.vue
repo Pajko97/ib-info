64a70fccb1a70c73e665304a230c4f424a70318d
pavle <pavle@git.topshipment.com>
2021-11-23 13:39:49 +0100

    Test

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -14,7 +14,7 @@
 
         <!-- primary nav -->
         <div class="hidden md:flex items-center space-x-1">
-          <nuxt-link :to="localePath('products')"><p href="#" class="py-5 px-3 text-white text-2xl hover:text-gray-900 hover:border-solid border-black">{{ $t('nav_products') }}</p></nuxt-link>
+          <nuxt-link to="/products"><p href="#" class="py-5 px-3 text-white text-2xl hover:text-gray-900 hover:border-solid border-black">{{ $t('nav_products') }}</p></nuxt-link>
           <nuxt-link :to="localePath('compatibility')"><p href="#" class="py-5 px-3 text-white text-2xl hover:text-gray-900 hover:border-solid border-black">{{ $t('nav_compatibility') }}</p></nuxt-link>
           <nuxt-link :to="localePath('/products/giftbox')"><p href="#" class="py-5 px-3 text-white text-2xl hover:text-gray-900">Gift box</p></nuxt-link>
           <nuxt-link :to="localePath('about')"><p href="#" class="py-5 px-3 text-white text-2xl hover:text-gray-900 hover:border-solid border-black">{{ $t('nav_about') }}</p></nuxt-link>
@@ -40,7 +40,7 @@
 
   <!-- mobile menu -->
   <div class="mobile-menu fixed bg-black" :class="this.menu_open ? 'visible z-50' : 'invisible'">
-      <nuxt-link :to="localePath('products')"><p href="#" class="text-center text-xl py-5 px-3 text-white hover:border-solid border-black">{{ $t('nav_products') }}</p></nuxt-link>
+      <nuxt-link to="/products"><p href="#" class="text-center text-xl py-5 px-3 text-white hover:border-solid border-black">{{ $t('nav_products') }}</p></nuxt-link>
           <nuxt-link :to="localePath('compatibility')"><p href="#" class="text-center text-xl py-5 px-3 text-white hover:border-solid border-black">{{ $t('nav_compatibility') }}</p></nuxt-link>
           <nuxt-link :to="localePath('/products/giftbox')"><p href="#" class="text-center text-xl py-5 px-3 text-white hover:border-solid border-black">Gift box</p></nuxt-link>
           <nuxt-link :to="localePath('about')"><p href="#" class="text-center text-xl py-5 px-3 text-white hover:border-solid border-black">{{ $t('nav_about') }}</p></nuxt-link>
